6c3d9ce7787159189d3b5c6d756b2efdcb88e031
tugbaustundag <tugbaustundag@outlook.com.tr>
2021-11-24 15:35:16 +0300

    9.ders bölüm1

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
-    //ıENtity implement eden class bir veritabanı tablosudur
+    //IEntity implement eden class bir veritabanı tablosudur
    public  interface IEntity
     {
     }
